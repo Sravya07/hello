c02406df57e6e2fe2b8d724ca84b2b27f93c5236
Sravya Somisetty <sravya.somisetty@sjsu.edu>
2020-03-09 13:29:55 -0500

    Hello Message App

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useRef, useState} from 'react';
 
 function App() {
+  var [message, setMsg] = useState()
+  const nameRef = useRef()
+  
+  
+  function handleClick(e){
+    const name = "Hello "+ nameRef.current.value
+    
+    setMsg(name)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input ref={nameRef} type="text" />
+      <button onClick={handleClick}> Click </button>
+      <p> {message} </p>
     </div>
   );
 }
